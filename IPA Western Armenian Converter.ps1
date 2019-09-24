e4a0e848b8cf4917427c2ba4d70c02b5b8e0dec7
asinno <alex.sinno@gmail.com>
2019-09-24 16:46:34 +0300

    First Commit

---
+++ IPA Western Armenian Converter.ps1
@@ -0,0 +1,54 @@
+﻿$DoubleIPAPairing = New-Object -TypeName psobject -Property @{
+
+'իւ'='ʏ';
+'էօ'='œ';
+'ու'='u';
+
+}
+
+
+$SingularIPAMap = New-Object -TypeName psobject -Property @{
+
+'ի' ='i';
+'իւ'='ʏ';
+'է'='ɛ';
+'ե'='ɛ'
+'էօ'='œ';
+'ը'='ə';
+'ա'='ɑ';
+'ու'='u';
+'ո'='o';
+'օ'='o';
+
+}
+
+
+
+[char[]]$String = ('իիւուէեէօըոօա')
+
+$count1 =0
+$count2 =1
+
+Clear-Variable array
+$Array = 
+foreach($character in $String){
+$letter =  $String[$count1] + $String[$count2]
+
+if($DoubleIPAPairing -notmatch $letter){
+$Map = ($SingularIPAMap | Select-Object -ExpandProperty $character)
+$letter = $character
+}else{
+$Map = ($DoubleIPAPairing | Select-Object -ExpandProperty $letter)
+}
+
+$StrungLetter = $letter | out-string
+$StrungLetter.replace("$letter","$Map")
+
+$count1++
+$count2++
+
+
+}
+
+$IPAWord = $array.Split('',[System.StringSplitOptions]::RemoveEmptyEntries) -join ""
+$IPAWord 
